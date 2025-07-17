8cf8100ddbbbbcb27f5919d2c46545826c6177d1
Vrushabh-13 <vrushabhgaikar2019@gmail.com>
2025-07-17 11:46:51 +0530

    Add files via upload

---
+++ Question1.js
@@ -0,0 +1,8 @@
+var sentense1 = "Hello, this is my first Node.js program!";
+var sentense2 = "I am excited to learn more about Node.js!";
+
+console.log(sentense1);
+console.log(sentense2);
+
+var fullsentence = sentense1 + " " + sentense2;
+console.log(fullsentence);
---
+++ Question2.js
@@ -0,0 +1,7 @@
+var sentense1 = "Hii, How are you?";
+console.log("Number of Characters:" +sentense1.length);
+var sentense2 = "Welcome to the world of technology.";
+console.log("Number of Characters:" +sentense2.length);
+var sentense3 = "Braves don't give excuses.";
+console.log("Number of Characters:" +sentense3.length);
+
